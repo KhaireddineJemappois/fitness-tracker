b825375715762c61228f671e4b0f200ced5e0122
Khaireddine MANSOURI <khaireddine.mansouri@smartiqaconsulting.be>
2023-03-26 04:59:07 +0200

    init

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button'
+import {MatInputModule} from '@angular/material/input'
+import {MatIconModule} from '@angular/material/icon'
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule
+  ],
+  exports:[
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule
+  ]
+})
+export class MaterialModule { }
