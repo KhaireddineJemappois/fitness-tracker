c49aa8f7662d873bab532f919160347e94080b37
Khaireddine MANSOURI <khaireddine.mansouri@smartiqaconsulting.be>
2023-04-03 00:03:35 +0200

    optimize unsubscribe

--- src/app/training/main/training.component.ts
+++ src/app/training/main/training.component.ts
@@ -1,4 +1,4 @@
-
+import { Subscription } from 'rxjs';
 import { TrainingService } from '../training.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 @Component({
@@ -10,13 +10,12 @@ export class TrainingComponent implements OnInit, OnDestroy {
   ongoingTraining = false;
   constructor(private trainingService: TrainingService) {}
   ngOnDestroy(): void {
+    this.exerciseSubs?.unsubscribe();
   }
-
+  exerciseSubs!: Subscription;
   ngOnInit(): void {
-    ;
-     this.trainingService.exerciseChanged?.subscribe(
+    this.exerciseSubs = this.trainingService.exerciseChanged?.subscribe(
       (exercise) => {
-        ;
         if (exercise) this.ongoingTraining = true;
         else this.ongoingTraining = false;
       }
